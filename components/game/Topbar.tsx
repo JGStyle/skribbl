b1d794e3c3ea05fd73daed6af69e2da910dd610d
JGStyle <jgs@jws.de>
2021-08-05 17:44:32 +0200

    extracted topbar and seperated it as single component (components/game/Topbar)

---
+++ components/game/Topbar.tsx
@@ -0,0 +1,31 @@
+import Btn from "../../components/default/Btn";
+import { variation as t } from "../../components/default/Btn";
+
+export default function Topbar({
+  time,
+  round,
+  roomname,
+}: {
+  time: number;
+  round: string;
+  roomname: string;
+}) {
+  return (
+    <div className="flex items-center bg-white text-white bg-opacity-10 justify-between rounded-2xl mb-4 py-2 px-8">
+      <div className="flex items-center">
+        <h3 className="font-semibold text-xl mr-3 p-2 bg-blue-500 rounded-full">
+          {time}s
+        </h3>
+        <h3 className="font-semibold text-xl">Round {round}</h3>
+      </div>
+      <h3 className="font-bold text-2xl">{roomname}</h3>
+      <Btn
+        type={t.Danger}
+        content="Leave"
+        click={() => {
+          console.log("leave");
+        }}
+      ></Btn>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,13 @@
 import CanvasUI from "../components/game/CanvasUI";
-import Btn from "../components/default/Btn";
-import { variation as t } from "../components/default/Btn";
 import Chat from "../components/chat/Chat";
 import Sidebar from "../components/players/Sidebar";
-import Messageinput from "../components/chat/Messageinput";
+import Topbar from "../components/game/Topbar";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen min-w-screen bg-main">
       <div className="flex flex-col">
-        <div className="flex items-center bg-white text-white bg-opacity-10 justify-between rounded-2xl mb-4 py-2 px-8">
-          <div className="flex items-center">
-            <h3 className="font-semibold text-xl mr-3 p-2 bg-blue-500 rounded-full">
-              45s
-            </h3>
-            <h3 className="font-semibold text-xl">Round 4/5</h3>
-          </div>
-          <h3 className="font-bold text-2xl">Insert Project name</h3>
-          <Btn
-            type={t.Danger}
-            content="Leave"
-            click={() => {
-              console.log("leave");
-            }}
-          ></Btn>
-        </div>
+        <Topbar roomname="Peters raum" time={30} round="3/5" />
         <div className="flex justify-center">
           <div className="mr-4 lg:block hidden">
             <Sidebar
