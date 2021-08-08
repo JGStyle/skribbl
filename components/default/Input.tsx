21df453e4f4af19faf5a8eb949db110fde38f5a3
JGStyle <jgs@jws.de>
2021-08-08 14:06:08 +0200

    improvements

--- components/default/Input.tsx
+++ components/default/Input.tsx
@@ -1,15 +1,18 @@
 export default function Input({
   placeholder,
   change,
+  value,
 }: {
   placeholder: string;
   change: (event: any) => void;
+  value: string;
 }) {
   return (
     <input
       type="text"
       placeholder={placeholder}
       onChange={change}
+      value={value}
       className="px-4 py-2 w-full rounded-xl focus:ring-4 focus:ring-blue-600 focus:border-transparent border border-transparent text-black"
     />
   );
