02ee1eae4e94c78c560a7c1a13394e8d4fad52c1
JGStyle <jgs@jws.de>
2021-12-16 16:07:11 +0100

    removed redundant code

---
+++ frontend/components/websockets/ws.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const isBrowser = typeof window !== "undefined";
+const url = "...";
+const [wsInstance] = useState(() => (isBrowser ? new WebSocket(url) : null));
+
+export default wsInstance;
